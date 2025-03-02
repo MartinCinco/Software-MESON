f6bc6a8a6d522d449e08652f018268b215378329
MartinCinco <jet5boymartin@gmail.com>
2025-03-02 11:10:44 -0700

    arreglado porque no jalaba el flask

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+"""
+Este módulo inicia una aplicación Flask simple para manejar rutas y renderizar plantillas.
+"""
 from flask import Flask, render_template
-##env\Scripts\activate
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """Ruta principal que renderiza index.html"""
     return render_template('index.html')
 
 if __name__ == '__main__':
